6a1fbbba46bd25c41d7b917e6b41fe9dda041bb1
skozawa <skozawa@hatena.ne.jp>
2017-07-09 16:19:55 +0900

    とりあえずslackのエンドポイントつくる

--- annotate/web.py
+++ annotate/web.py
@@ -3,10 +3,23 @@ from flask_restful import Resource
 from annotate import api
 from annotate import app
 
+
 class Root(Resource):
     """dispatch /."""
     def get(self):
         return {'message': 'Hello'}
 
 
+class SlackAction(Resource):
+    def post(self):
+        return {}
+
+
+class SlackOptions(Resource):
+    def post(self):
+        return {}
+
+
 api.add_resource(Root, '/')
+api.add_resource(SlackAction, '/slack/action')
+api.add_resource(SlackOption, '/slack/options')

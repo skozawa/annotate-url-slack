252598a8f754b4206631167c5c5bf198cc987acc
skozawa <skozawa@hatena.ne.jp>
2017-07-17 16:17:05 +0900

    yes / noを受け取るように

--- annotate/resources/slack_action.py
+++ annotate/resources/slack_action.py
@@ -1,15 +1,28 @@
 import json
 
 from flask_restful import Resource
-
 from flask_restful import reqparse
+
+import annotate.slack.request as slack_req
 
 class SlackAction(Resource):
     def post(self):
+        payload = self.get_payload()
+        print(payload)
+        req = self.payload_to_request(payload)
+        if req.action_is_ok:
+            return {'text': 'yes'}
+        else:
+            return {'text': 'no'}
+
+    def get_payload(self):
         parser = reqparse.RequestParser()
         parser.add_argument('payload', required=True, type=json.loads)
         args = parser.parse_args()
-        print(args)
-        return {}
+        return args['payload']
 
-    
+    def payload_to_request(self, payload):
+        callback_id = payload.get('callback_id', None)
+        if callback_id is not None:
+            return
+        return slack_req.ActionRequest(payload)
---
+++ annotate/slack/request.py
@@ -0,0 +1,63 @@
+from datetime import datetime
+
+class ActionRequest(object):
+    def __init__(self, payload):
+        self.payload = payload
+
+    @property
+    def actions(self):
+        return self.payload.get('actions', [])
+
+    def action_is_ok(self):
+        actions = self.actions
+        if len(actions) == 1 and actions[0].get('value', '') == 'yes':
+            return True
+        return False
+
+    @property
+    def callback_id(self):
+        return self.payload.get('callback_id')
+
+    @property
+    def team_id(self):
+        return self.payload.get('team', {}).get('id')
+
+    @property
+    def team_domain(self):
+        return self.payload.get('team', {}).get('domain')
+
+    @property
+    def channel_id(self):
+        return self.payload.get('channel', {}).get('id')
+
+    @property
+    def channel_name(self):
+        return self.payload.get('channel', {}).get('name')
+
+    @property
+    def user_id(self):
+        return self.payload.get('user', {}).get('id')
+
+    @property
+    def user_name(self):
+        return self.payload.get('user', {}).get('name')
+
+    @property
+    def action_dt(self):
+        if self._action_dt is None:
+            self._action_dt = datetime.fromtimestamp(self.payload.get('action_ts', 0))
+        return self._action_dt
+
+    @property
+    def message_dt(self):
+        if self._message_dt is None:
+            self._message_dt = datetime.fromtimestamp(self.payload.get('message_ts', 0))
+        return self._message_dt
+
+    @property
+    def original_message(self):
+        return self.payload.get('original_message', {})
+
+    @property
+    def response_url(self):
+        return self.payload.get('response_url')

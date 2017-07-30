4cd40f14a15f24bf105c45d0663aa80cfb5520af
skozawa <shunsuke.kozawa@gmail.com>
2017-07-30 18:19:06 +0900

    シートの初期化

--- annotate/spread.py
+++ annotate/spread.py
@@ -15,12 +15,15 @@ class Gspread(object):
             ]
         )
         self.client = gspread.authorize(credentials)
+        today = datetime.date.today()
+        self.year = today.year
+        self.month = today.month
         self._current_spreadsheet = None
         
     @property
     def current_spreadsheet(self):
         if self._current_spreadsheet is None:
-            name = 'annotate-quality-%s' % (datetime.datetime.now().strftime('%y%m'))
+            name = 'annotate-quality-%04d%02d' % (self.year, self.month)
             self._current_spreadsheet = self.open_or_create_spreadsheet(name)
         return self._current_spreadsheet
         
@@ -29,5 +32,16 @@ class Gspread(object):
             sheet = self.client.open(name)
         except Exception:
             sheet = self.client.create(name)
-
+            self.initialize_sheet(sheet)
         return sheet
+
+    def initialize_sheet(self, sheet):
+        current_worksheets = sheet.worksheets()
+        columns = ['URL', 'Time', 'Title', 'Annotator'] + ['Quality', 'Readability', 'Informativeness', 'Style', 'Topic', 'Sentiment']
+        for ws_index in range(12):
+            ws = sheet.add_worksheet(title='%02d' % (ws_index + 1), rows=1, cols=len(columns))
+            for (i, value) in enumerate(columns):
+                ws.update_cell(1, i+1, value)
+        for worksheet in current_worksheets:
+            sheet.del_worksheet(worksheet)
+

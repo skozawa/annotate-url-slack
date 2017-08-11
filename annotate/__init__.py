20313df7321e973b248dd9c060b51fca92cbd824
skozawa <shunsuke.kozawa@gmail.com>
2017-08-11 15:44:41 +0900

    add method for connecting db

--- annotate/__init__.py
+++ annotate/__init__.py
@@ -1,13 +1,27 @@
 from flask import Flask
+from flask import g
 
 from flask_restful import Api
 
 from annotate.config import config
+from annotate.db import DB
 
 
 app = Flask(__name__)
 app.config.from_object(config)
 
 api = Api(app)
+
+def connect_db():
+    db = getattr(g, '_db', None)
+    if db is None:
+        db = g._db = DB()
+    return db
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    db = getattr(g, '_db', None)
+    if db is not None:
+        db.close()
 
 import annotate.web
